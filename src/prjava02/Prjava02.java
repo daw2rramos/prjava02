91324d3fa18a68f7234852b83677f4457eda8c31
daw2rramos <rramosfred@gmail.com>
2014-04-30 11:20:48 +0200

    Treballant amb ordres Git i branques v0.1

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -1,22 +1,26 @@
 /*
-To change this template, choose Tools | Templates 
-and open the template in the editor. 
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package prjava02;
-import java.io.*; import java.net.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
 /**
-* @author dc1314daw@gmail.com */
+ *
+ * @author alumne
+ */
 public class Prjava02 {
-/**
-* @param args the command line arguments */
-    public static void main(String[] args) 
-    { 
-        try {
-            InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
-            String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
-            System.out.println("Nom de l'usuari : " + System.getProperty("user.name")); System.out.println("Carpeta Personal: " + System.getProperty("user.home")); System.out.println("Sistema operatiu: " + System.getProperty("os.name")); System.out.println("Versió OS: " + System.getProperty("os.version"));
-System.out.println("Arquitecura OS: " + System.getProperty("os.arch"));
+
+    public static void main(String[] args) { try {
+        InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
+        String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
+        System.out.println("Nom de l'usuari: " + System.getProperty("user.name")); System.out.println("Carpeta Personal: " + System.getProperty("user.home")); System.out.println("Sistema operatiu: " + System.getProperty("os.name")); System.out.println("Versió OS: " + System.getProperty("os.version"));
         }
         catch (IOException e) { e.printStackTrace();}
     }
+    
 }
